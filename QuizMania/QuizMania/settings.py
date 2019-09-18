8a7a21d329de9e502c84c3685c29f4046cbcd597
sharmapradyumn <47988611+sharmapradyumn@users.noreply.github.com>
2019-09-18 22:43:25 +0530

    login_register work completed

--- QuizMania/QuizMania/settings.py
+++ QuizMania/QuizMania/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'login_register.apps.LoginRegisterConfig',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'QuizMania.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["QuizMania_V2/templates",],
+        'DIRS': ["QuizMania_V2/templates","login_register/templates1"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

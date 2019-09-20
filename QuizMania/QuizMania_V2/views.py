9f042395f44874ab46785ec16ad5dcd17890513a
piyush <piyushagarwal.0108@gmail.com>
2019-09-20 16:01:17 +0530

    new front page added

--- QuizMania/QuizMania_V2/views.py
+++ QuizMania/QuizMania_V2/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,"QuizMania_V2/index.html")
+    return render(request,"QuizMania_V2/Catalog.html")
 
 def admin_V(request):
     return render(request,"QuizMania_V2/admin.html")
